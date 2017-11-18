9f2dd9166748640a1999bc1699ef473398e284b7
Iluvatar <wfoquendop@unal.edu.co>
2017-11-18 16:18:23 -0500

    Updated bindings for shrinking windows

--- common/.emacs.d/init.el
+++ common/.emacs.d/init.el
@@ -430,10 +430,14 @@
   ;;(global-set-key (kbd "<M-down>") 'windmove-down)
   ;;(global-set-key (kbd "<M-left>") 'windmove-left)
   ;;(global-set-key (kbd "<M-right>") 'windmove-right)
-  (global-set-key ((kbd "") "S-C-<left>") 'shrink-window-horizontally)
-  (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
-  (global-set-key (kbd "S-C-<down>") 'shrink-window)
-  (global-set-key (kbd "S-C-<up>") 'enlarge-window)
+  ;; (global-set-key ((kbd "") "S-C-<left>") 'shrink-window-horizontally)
+  ;; (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+  ;; (global-set-key (kbd "S-C-<down>") 'shrink-window)
+  ;; (global-set-key (kbd "S-C-<up>") 'enlarge-window)
+  (global-set-key (kbd "C-c <left>")  'windmove-left)
+  (global-set-key (kbd "C-c <right>") 'windmove-right)
+  (global-set-key (kbd "C-c <up>")    'windmove-up)
+  (global-set-key (kbd "C-c <down>")  'windmove-down)
   )
 
 
