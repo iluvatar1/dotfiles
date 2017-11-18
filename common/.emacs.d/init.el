b9a3fc0dd5297747775cdbf3e28af279d8110b79
Iluvatar <wfoquendop@unal.edu.co>
2017-11-18 15:17:45 -0500

    Added counter to measure start time
    This measures the time to load init.el

--- common/.emacs.d/init.el
+++ common/.emacs.d/init.el
@@ -13,6 +13,8 @@
 ;;   )
 
 
+(defvar my-start-time (current-time)
+"Time when Emacs was started")
 
 ;; Accept ad-redefinition warnings
 ;; from:http://andrewjamesjohnson.com/suppressing-ad-handle-definition-warnings-in-emacs/
@@ -1456,10 +1458,13 @@ executable.")
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (helm-themes zenburn-theme yasnippet yaml-mode writegood-mode which-key w3m use-package underwater-theme symon solarized-theme smex smartparens smart-mode-line selected rainbow-delimiters powerline pov-mode pdf-tools page-break-lines outshine osx-clipboard org-pomodoro org-gcal names multi-term moe-theme magit magic-latex-buffer leuven-theme latex-extra hungry-delete htmlize helm-pages helm-descbinds helm-bibtex gnuplot git-gutter-fringe ggtags flycheck fill-column-indicator fic-mode exec-path-from-shell esup ein ecb dracula-theme default-text-scale cuda-mode company-irony color-theme color-identifiers-mode clang-format cdlatex beacon autopair auto-package-update auctex-latexmk aggressive-indent))))
+    (diminish helm-themes zenburn-theme yasnippet yaml-mode writegood-mode which-key w3m use-package underwater-theme symon solarized-theme smex smartparens smart-mode-line selected rainbow-delimiters powerline pov-mode pdf-tools page-break-lines outshine osx-clipboard org-pomodoro org-gcal names multi-term moe-theme magit magic-latex-buffer leuven-theme latex-extra hungry-delete htmlize helm-pages helm-descbinds helm-bibtex gnuplot git-gutter-fringe ggtags flycheck fill-column-indicator fic-mode exec-path-from-shell esup ein ecb dracula-theme default-text-scale cuda-mode company-irony color-theme color-identifiers-mode clang-format cdlatex beacon autopair auto-package-update auctex-latexmk aggressive-indent)))
+ '(reb-re-syntax (quote string)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(message "Start up time %.2fs" (float-time (time-subtract (current-time) my-start-time)))
