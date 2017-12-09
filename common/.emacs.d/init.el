5dc994253938b9c72d856c3997181a2c5ce5dfb0
Iluvatar <wfoquendop@unal.edu.co>
2017-12-09 11:57:58 -0500

    Added a more complete code for org babel init load

--- common/.emacs.d/init.el
+++ common/.emacs.d/init.el
@@ -38,7 +38,39 @@
 
 
 ;; load my init org file
+;; From : http://orgmode.org/worg/org-contrib/babel/intro.html
+(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+
+(let* ((org-dir (expand-file-name
+                 "lisp" (expand-file-name
+                         "org" (expand-file-name
+                                "src" dotfiles-dir))))
+       (org-contrib-dir (expand-file-name
+                         "lisp" (expand-file-name
+                                 "contrib" (expand-file-name
+                                            ".." org-dir))))
+       (load-path (append (list org-dir org-contrib-dir)
+                          (or load-path nil))))
+  ;; load up Org-mode and Org-babel
+  (require 'org-install)
+  (require 'ob-tangle))
+
+;; load the org file
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 
 
 (message "Start up time %.2fs" (float-time (time-subtract (current-time) my-start-time)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (smartparens-html expand-region zenburn-theme yasnippet yaml-mode writegood-mode which-key web-mode w3m use-package underwater-theme try symon solarized-theme smex smartparens smart-mode-line selected rainbow-delimiters powerline pov-mode pdf-tools pcre2el page-break-lines outshine osx-clipboard org-pomodoro org-gcal org-bullets names multi-term moe-theme magit magic-latex-buffer leuven-theme latex-extra hydra hungry-delete htmlize highlight-symbol highlight-parentheses helm-themes helm-pages helm-descbinds helm-bibtex gnuplot git-gutter-fringe ggtags flycheck fill-column-indicator fic-mode exec-path-from-shell esup ein ecb dracula-theme dired+ default-text-scale cuda-mode company-irony color-theme color-identifiers-mode clang-format cdlatex beacon autopair auto-package-update auto-compile auctex-latexmk aggressive-indent))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
