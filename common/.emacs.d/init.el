8f4fa4e528f6744fc04d8709417e150cc642c3e0
Iluvatar <woquendo@gmail.com>
2020-11-18 19:20:54 -0500

    doom config

--- common/.emacs.d/init.el
+++ common/.emacs.d/init.el
@@ -1,79 +1,49 @@
-;; Add horizontal rulers with Ctrl-q Ctrl-l
-(defvar my-start-time (current-time)
-  "Time when Emacs was started")
+;;; init.el -*- lexical-binding: t; -*-
+;;
+;; Author:  Henrik Lissner <henrik@lissner.net>
+;; URL:     https://github.com/hlissner/doom-emacs
+;;
+;;   =================     ===============     ===============   ========  ========
+;;   \\ . . . . . . .\\   //. . . . . . .\\   //. . . . . . .\\  \\. . .\\// . . //
+;;   ||. . ._____. . .|| ||. . ._____. . .|| ||. . ._____. . .|| || . . .\/ . . .||
+;;   || . .||   ||. . || || . .||   ||. . || || . .||   ||. . || ||. . . . . . . ||
+;;   ||. . ||   || . .|| ||. . ||   || . .|| ||. . ||   || . .|| || . | . . . . .||
+;;   || . .||   ||. _-|| ||-_ .||   ||. . || || . .||   ||. _-|| ||-_.|\ . . . . ||
+;;   ||. . ||   ||-'  || ||  `-||   || . .|| ||. . ||   ||-'  || ||  `|\_ . .|. .||
+;;   || . _||   ||    || ||    ||   ||_ . || || . _||   ||    || ||   |\ `-_/| . ||
+;;   ||_-' ||  .|/    || ||    \|.  || `-_|| ||_-' ||  .|/    || ||   | \  / |-_.||
+;;   ||    ||_-'      || ||      `-_||    || ||    ||_-'      || ||   | \  / |  `||
+;;   ||    `'         || ||         `'    || ||    `'         || ||   | \  / |   ||
+;;   ||            .===' `===.         .==='.`===.         .===' /==. |  \/  |   ||
+;;   ||         .=='   \_|-_ `===. .==='   _|_   `===. .===' _-|/   `==  \/  |   ||
+;;   ||      .=='    _-'    `-_  `='    _-'   `-_    `='  _-'   `-_  /|  \/  |   ||
+;;   ||   .=='    _-'          '-__\._-'         '-_./__-'         `' |. /|  |   ||
+;;   ||.=='    _-'                                                     `' |  /==.||
+;;   =='    _-'                                                            \/   `==
+;;   \   _-'                                                                `-_   /
+;;    `''                                                                      ``'
+;;
+;; These demons are not part of GNU Emacs.
+;;
+;;; License: MIT
 
-;; (setq use-package-verbose t) ;; make startup slower
-;; (setq load-prefer-newer t)
+;; A big contributor to startup times is garbage collection. We up the gc
+;; threshold to temporarily prevent it from running, then reset it later by
+;; enabling `gcmh-mode'. Not resetting it will cause stuttering/freezes.
+(setq gc-cons-threshold most-positive-fixnum)
 
-;; From: https://writequit.org/org/settings.html
-;; keep customize settings in their own file
-(setq custom-file "~/.emacs.d/custom.el")
-(when (file-exists-p custom-file)
-  (load custom-file))
+;; In noninteractive sessions, prioritize non-byte-compiled source files to
+;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
+;; to skip the mtime checks on every *.elc file.
+(setq load-prefer-newer noninteractive)
 
-;; from http://cachestocaches.com/2015/8/getting-started-use-package/
-;; http://cestlaz.github.io/posts/using-emacs-10-org-init/
-(require 'package)
-(setq package-enable-at-startup nil)
-;;(add-to-list 'package-archives '("melpa-stb" . "https://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
-(package-initialize)
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(eval-when-compile
-  (require 'use-package))
-(use-package diminish
-  :ensure t)
-(use-package bind-key
-  :ensure t)
-(package-refresh-contents t) ;; runs in background
+(let (file-name-handler-alist)
+  ;; Ensure Doom is running out of this file's directory
+  (setq user-emacs-directory (file-name-directory load-file-name)))
 
-;;https://github.com/rranelli/auto-package-update.el
-;; run: M-x auto-package-update-now
-(use-package auto-package-update
-  :defer 5)
+;; Load the heart of Doom Emacs
+(load (concat user-emacs-directory "core/core")
+      nil 'nomessage)
 
-;;; from http://cestlaz.github.io/posts/using-emacs-1-setup/
-;;(use-package try
-;;  :ensure t)
-
-;; Organizing packages automatically
-;;https://www.reddit.com/r/emacs/comments/4fqu0a/automatically_install_packages_on_startup/
-(setq use-package-always-ensure t)
-
-
-;; load my init org file
-;; ;; From : http://orgmode.org/worg/org-contrib/babel/intro.html
-;; (setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
-;; (let* ((org-dir (expand-file-name
-;;                  "lisp" (expand-file-name
-;;                          "org" (expand-file-name
-;;                                 "src" dotfiles-dir))))
-;;        (org-contrib-dir (expand-file-name
-;;                          "lisp" (expand-file-name
-;;                                  "contrib" (expand-file-name
-;;                                             ".." org-dir))))
-;;        (load-path (append (list org-dir org-contrib-dir)
-;;                           (or load-path nil))))
-;;   ;; load up Org-mode and Org-babel
-;;   (require 'org-install)
-;;   (require 'ob-tangle))
-;; ;; load the org file
-;; (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
-
-;;From https://writequit.org/org/settings.html
-;; (require 'org)
-;; (require 'ob-tangle)
-;; (org-babel-load-file
-;;  (expand-file-name "myinit.org"
-;; 		   user-emacs-directory))
-;; From https://raw.githubusercontent.com/zamansky/using-emacs/master/myinit.org
-(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
-
-;; TP RELOAD I NEED T REMOVE myinit.el
-
-(message "Start up time %.2fs" (float-time (time-subtract (current-time) my-start-time)))
-
+;; And let 'er rip!
+(doom-initialize)
