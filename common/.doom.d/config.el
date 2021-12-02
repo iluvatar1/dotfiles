eff5a75a20ca2a4c9b9a0e5cb002617e50909aa6
Iluvatar <woquendo@gmail.com>
2021-12-02 13:22:38 -0500

    Updated doom config

--- common/.doom.d/config.el
+++ common/.doom.d/config.el
@@ -1,16 +1,24 @@
 (setq user-full-name "William Oquendo"
       user-mail-address "woquendo@gmail.com")
-;;(setq doom-theme 'doom-one)
-(setq org-directory "~/Dropbox/TODO/")
+(setq confirm-kill-emacs nil)
 (setq display-line-numbers-type t)
-(add-hook 'org-mode-hook 'turn-on-auto-fill)
-(add-hook 'text-mode-hook 'turn-on-auto-fill)
+;;(setq doom-theme 'doom-one)
 (setq doom-theme 'modus-vivendi)
+;;(setq doom-theme 'spacemacs-light)
+(custom-set-faces!
+  '(doom-dashboard-banner :inherit default)
+  '(doom-dashboard-loaded :inherit default))
+;;(setq spacemacs-theme-comment-bg nil)
+
+
 (add-to-list 'exec-path "/usr/local/bin/")
 ;;(setq ccls-executable "/usr/local/bin/ccls")
 (menu-bar-mode 1)
 
-(add-hook 'org-mode-hook 'turn-on-auto-fill)
+(setq org-directory "~/Dropbox/TODO/")
+(add-hook! org-mode (electric-indent-local-mode -1))
+(add-hook! 'org-mode-hook 'turn-on-auto-fill)
+(add-hook! 'text-mode-hook 'turn-on-auto-fill)
 (setq org-latex-image-default-width ".45\\textwidth")
 (setq org-latex-images-centered nil)
 (setq org-latex-listings 'minted) ;; colored latex
@@ -34,20 +42,36 @@
 ;;(setq org-latex-pdf-process ("pdflatex --shell-escape %f"))
 (setq org-latex-pdf-process '("latexmk -pdflatex='pdflatex -shell-escape  -interaction nonstopmode' -pdf -bibtex -f %f"))
 (setq org-latex-to-pdf-process '("latexmk -pdflatex='pdflatex -shell-escape  -interaction nonstopmode' -pdf -bibtex -f %f"))
+(defun zz/adjust-org-company-backends ()
+  (remove-hook 'after-change-major-mode-hook '+company-init-backends-h)
+  (setq-local company-backends nil))
+(add-hook! org-mode (zz/adjust-org-company-backends))
+(setq org-reveal-mathjax t)
+(use-package! htmlize
+:ensure t)
 
-(require 'eglot)
-(add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
+;;(require 'eglot)
+;;(add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
 (add-hook 'c-mode-hook 'eglot-ensure)
 (add-hook 'c++-mode-hook 'eglot-ensure)
+(set-eglot-client! 'cc-mode '("clangd" "-j=3" "--clang-tidy"))
+(set-eglot-client! 'c-mode '("clangd" "-j=3" "--clang-tidy"))
+(set-eglot-client! 'c++-mode '("clangd" "-j=3" "--clang-tidy"))
+(after! cc-mode (setq c-basic-offset 2)) ; set it globally
+(setq-hook! 'c++-mode-hook c-basic-offset 2) ; set it in c++-mode buffers
 
 ;; ~/.doom.d/config.el
 (setq +python-ipython-repl-args '("-i" "--simple-prompt" "--no-color-info"))
 (setq +python-jupyter-repl-args '("--simple-prompt"))
 
-(require 'multiple-cursors)
+;;(require 'multiple-cursors)
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
 (setq org-image-actual-width 'nil)
+
+
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-clang-tidy-setup))
--- common/.doom.d/init.el
+++ common/.doom.d/init.el
@@ -1,62 +1,87 @@
+;; see https://zzamboni.org/post/my-doom-emacs-configuration-with-commentary/
 (doom! :completion
-        company
-        ivy
-	irony
-        :ui
-        doom
-        doom-dashboard
-        ;doom-quit
-        hl-todo
-        hydra
-        minimap
-        modeline
-        ;ophints
-        ;(popup +defaults)
-        neotree
-        treemacs
-        :editor
-        file-templates
-        fold
-        snippets
-        :emacs
-        dired
-        electric
-        undo
-        ;:term
-        ;vterm
-        :checkers
-        ;syntax
-        :tools
-        (eval +overlay)
-        ;;lsp
-        magit
-        make
-        ;;pdf
-        :lang 
-        (cc +irony)
-        emacs-lisp
-        (latex
-	 +latexmk
-	 +cdlatex
-	 +fold)
-        markdown
-        (org
-	 +pretty
-	 +dragndrop
-	 +jupyter
-	 +pandoc
-	 +gnuplot
-	 +pomodoro
-	 +present)
-        ;(python +lsp +conda)
-        (python +conda +pyright)
-        sh
-	yaml
-        :email
-	(:if (executable-find "mu") (mu4e +org +gmail))
-        :app
-        calendar
-	(rss +org)
-        :config
-        (default +smartparens)
-)
+       (company)
+       (ivy)
+       ;;(ivy +prescient -childframe -fuzzy +icons)
+       ;;irony
+
+       :ui
+       doom
+       ;;doom-dashboard
+       ;;doom-quit
+       hl-todo
+       ;;hydra
+       ;;minimap
+       ;;modeline
+       ;;ophints
+       ;;(popup +defaults)
+       neotree
+       ;;treemacs
+       ;;(ligature +extras)
+       ;;nav-flash
+       ;;ophints ; highlight the region an operation acts on
+       ;;zen                   ; distraction-free coding or writing
+       ;;window-select         ; visually switch windows
+       
+       :editor
+       file-templates
+       ;; fold
+       snippets
+       ;;clang-format
+       ;;(format +onsave)    ; automated prettiness
+       multiple-cursors    ; editing in many places at once
+
+       :emacs
+       dired
+       ;;electric
+       undo
+
+       ;;:term
+       ;;vterm
+
+       :checkers
+       ;;(syntax +childframe)  ; tasing you for every semicolon you forget
+       ;;spell                 ; tasing you for misspelling mispelling
+
+       :tools
+       (eval +overlay)
+       (lsp +eglot)
+       magit
+       ;;pdf
+       (debugger +lsp)              ; FIXME stepping through code, to help you add bugs
+       ;;lookup                ; navigate your code and its documentation
+       ;;(magit +forge)        ; a git porcelain for Emacs
+       make                ; run make tasks from Emacs
+
+       :lang 
+       (cc +lsp)
+       ;;emacs-lisp
+       ;;(latex
+       ;; +latexmk
+       ;; +cdlatex
+       ;; +fold)
+       markdown
+       (org +present +gnuplot +pandoc)
+       ;;(org
+       ;; +pretty
+       ;; +dragndrop
+       ;; +jupyter
+       ;; +pandoc
+       ;; +gnuplot
+       ;; +pomodoro
+       ;; +present)
+       (python +lsp)
+       ;;(python +conda +pyright)
+       (sh +lsp)
+       yaml
+
+       :email
+       (:if (executable-find "mu") (mu4e +org +gmail))
+
+       :app
+       ;;calendar
+       ;;(rss +org)
+
+       :config
+       (default +smartparens)
+       )
